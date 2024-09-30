065604cfc250528dd7e2ca3843af713ff49a3981
ashraf56 <ashrafulfahim07@gmail.com>
2024-09-30 15:29:15 +0600

    user controllers crated

---
+++ src/app/module/user/user.controller.ts
@@ -0,0 +1,55 @@
+import httpStatus from "http-status";
+import { tryCatchWrapper } from "../../utills/tryCatchWrapper";
+import { UsersService } from "./user.service";
+
+const RegisterUserController = tryCatchWrapper(
+    async (req, res) => {
+        const payload = req.body;
+        const result = await UsersService.RegisterUserDB(payload)
+
+        res.status(httpStatus.OK).json({
+            success: true,
+            statusCode: httpStatus.OK,
+            message: "User registered successfully",
+            data: result
+        })
+    }
+)
+const LoginUserController = tryCatchWrapper(
+    async (req, res) => {
+        const payload = req.body;
+        const result = await UsersService.logIn(payload)
+
+        res.status(httpStatus.OK).json({
+            success: true,
+            statusCode: httpStatus.OK,
+            message: "Logged in successfully",
+            data: result.users,
+            token: result.token
+        })
+    }
+)
+const updateUserController = tryCatchWrapper(
+    async (req, res) => {
+        const payload = req.body;
+        const { id } = req.params
+        const result = await UsersService.updateUserDB(id, payload)
+
+        res.status(httpStatus.OK).json({
+            success: true,
+            statusCode: httpStatus.OK,
+            message: "User info updated successfully",
+            data: result,
+
+        })
+    }
+)
+
+
+
+
+export const UserControllers = {
+    RegisterUserController,
+    LoginUserController,
+    updateUserController
+}
